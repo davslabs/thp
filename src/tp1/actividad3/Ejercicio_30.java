20eb3a96d5d73caa0dace40d778e5f57c87d4f90
David Ezequiel Assen <assen.david@gmail.com>
2021-08-25 20:32:26 -0300

    agregado ej30

--- src/tp1/actividad3/Ejercicio_30.java
+++ src/tp1/actividad3/Ejercicio_30.java
@@ -1,10 +1,66 @@
 package tp1.actividad3;
 
+import java.util.Scanner;
+
 public class Ejercicio_30 {
 
+	public static Scanner input = new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num1;
+		int num2;
+		char operacion;
+		
+		do {
+			System.out.println("Ingrese un caracter para la operacion: [+,-,*,/,'F' para salir]");
+			operacion = input.nextLine().toUpperCase().charAt(0);
+		} while(
+			operacion != '+' &&
+			operacion != '-' &&
+			operacion != '*' &&
+			operacion != '/' &&
+			operacion != 'F');
+		
+		while(operacion != 'F') {
+			System.out.println("Ingrese numero 1: ");
+			num1 = Integer.parseInt(input.nextLine());
+			
+			System.out.println("Ingrese numero 2: ");
+			num2 = Integer.parseInt(input.nextLine());
+			
+			switch(operacion) {
+				case '+':
+					System.out.println("El resultado de la operacion es " + (num1+num2));
+					break;
+				case '-':
+					System.out.println("El resultado de la operacion es " + (num1-num2));
+					break;
+				case '*':
+					System.out.println("El resultado de la operacion es " + (num1*num2));
+					break;
+				case '/':
+					if(num2 == 0) {
+						System.out.println("ERROR");
+					} else {
+						System.out.println("El resultado de la operacion es " + ((double)num1/(double)num2)); 
+					}
+					break;
+				default:
+					System.out.println("Operacion invalida");
+					break;
+			}
+			
+			do {
+				System.out.println("Ingrese un caracter para la operacion: [+,-,*,/,'F' para salir]");
+				operacion = input.nextLine().toUpperCase().charAt(0);
+			} while(
+				operacion != '+' &&
+				operacion != '-' &&
+				operacion != '*' &&
+				operacion != '/' &&
+				operacion != 'F');	
+		}
 
+		input.close();
 	}
 
 }
