d45354b98e1da59a5595a66c784dfb6c95cb67ba
David Ezequiel Assen <assen.david@gmail.com>
2021-08-25 22:33:28 -0300

    agregado 32

--- src/tp1/actividad3/Ejercicio_32.java
+++ src/tp1/actividad3/Ejercicio_32.java
@@ -1,10 +1,23 @@
 package tp1.actividad3;
 
+import java.util.Scanner;
+
 public class Ejercicio_32 {
 
+	public static Scanner input = new Scanner(System.in);
+	public static final int NOTA_MAX = 10;
+	public static final int NOTA_MIN = 4;
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int nota;
+		do {
+			System.out.println("Ingrese una nota:");
+			nota = Integer.parseInt(input.nextLine());
+		} while(nota < NOTA_MIN && nota > NOTA_MAX && nota != 2);
+		System.out.println("** NOTA INGRESADA CORRECTAMENTE");
+		System.out.println("La nota es: " + nota);
+		
+		input.close();
 	}
 
 }
